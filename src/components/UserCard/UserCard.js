8bd977aea295725ec1cfd49cda56756c595ad3d3
Todor Vlasev <todor.vlasev@strypes.eu>
2019-09-28 00:35:57 +0300

    adding leader board page and sorting users depends on their rank

---
+++ src/components/UserCard/UserCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './UserCard.css'
+
+const UserCard = ({user, userAnswers, userQuestions, userScore, rank}) => {
+  return(
+    <div className="user-card-container">
+      <img className="user-img" alt="user-img" src={user.avatarURL} />
+      <div style={{padding: '5px'}}>
+        <h2 style={{margin: '5px'}}>{user.name}</h2>
+        <p>Rank: {rank}</p>
+        <p>Answered Questions: {userAnswers}</p>
+        <p>Asked Questions: {userQuestions}</p>
+        <hr />
+        <p>Score: {userScore}</p>
+      </div>
+    </div>
+  )
+}
+
+export default UserCard
--- src/containers/LeaderBoard/LeaderBoard.js
+++ src/containers/LeaderBoard/LeaderBoard.js
@@ -1,13 +1,52 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getUsers } from '../../actions/Users'
+import UserCard from "../../components/UserCard/UserCard"
+import "./LeaderBoard.css"
 
 class LeaderBoard extends Component {
+
+  componentDidMount = () => {
+    this.props.getUsers()
+  }
+
   render(){
+    const { users } = this.props
+    const allUsers = Object.values(users)
+
     return(
-      <Fragment>
-        LeaderBoard page
-      </Fragment>
+      <div className="users-container">
+        {
+          allUsers.sort((a, b) => (Object.keys(a.answers).length + a.questions.length) < (Object.keys(b.answers).length + b.questions.length) ? 1 : -1)
+            .map((user,i) => (
+              <UserCard 
+                key={user.id}
+                rank={i+1}
+                user={user} 
+                userAnswers={Object.keys(user.answers).length}
+                userQuestions={user.questions.length}
+                userScore={Object.keys(user.answers).length + user.questions.length}
+                users={allUsers}
+              />
+            ))
+        }
+      </div>
     )
   }
 }
 
-export default LeaderBoard
+const mapStateToProps = state => {
+  return {
+    users: state.getUsers.users,
+    isPendingUsers: state.getUsers.isPending,
+    errorUsers: state.getUsers.error,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getUsers: () => dispatch(getUsers())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LeaderBoard);
