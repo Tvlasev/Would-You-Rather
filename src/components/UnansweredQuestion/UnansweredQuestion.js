e5e27c954b8521e91806bbd6b003fb35d4d25a2d
Todor Vlasev <todor.vlasev@strypes.eu>
2019-09-28 05:52:24 +0300

    adding state to unanswered component

--- src/components/UnansweredQuestion/UnansweredQuestion.js
+++ src/components/UnansweredQuestion/UnansweredQuestion.js
@@ -1,31 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Radio, RadioGroup, FormControlLabel, Button } from '@material-ui/core/'
 import "./UnansweredQuestion.css"
 
-const UnansweredQuestion = ({optionOne, optionTwo}) => {
-  return(
-    <div>
-      <div className="question-body">
-        <RadioGroup>
-        <FormControlLabel
-            value="option1"
-            control={<Radio color="primary" />}
-            label={optionTwo}
-            labelPlacement="end"
-          />
+class UnansweredQuestion extends Component{
+
+  state = {
+    option: ""
+  }
+
+  handleAnswer = e => this.setState({...this.state, option: e.target.value})
+
+  render(){
+    const {optionOne, optionTwo} = this.props
+    
+    return(
+      <div>
+        <div className="question-body">
+          <RadioGroup>
           <FormControlLabel
-            value="option2"
-            control={<Radio color="primary" />}
-            label={optionOne}
-            labelPlacement="end"
-          />
-        </RadioGroup>
+              value="option1"
+              control={<Radio color="primary" />}
+              label={optionTwo}
+              labelPlacement="end"
+              onChange={(e) => this.handleAnswer(e)}
+            />
+            <FormControlLabel
+              value="option2"
+              control={<Radio color="primary" />}
+              label={optionOne}
+              labelPlacement="end"
+              onChange={(e) => this.handleAnswer(e)}
+            />
+          </RadioGroup>
+        </div>
+        <div className="answer-button">
+          <Button disabled={this.state.option === ''} fullWidth variant="contained" color='primary'>Answer</Button>
+        </div>
       </div>
-      <div className="answer-button">
-        <Button fullWidth variant="contained" color='primary'>Answer</Button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default UnansweredQuestion
